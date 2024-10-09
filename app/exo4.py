b708e43b43f4cc4ccdef9563ee7c599cd03a054d
Romain <romain.chaumont@hotmail.fr>
2024-10-09 11:34:26 +0200

    chiant

--- app/exo4.py
+++ app/exo4.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count, sum, window
+from pyspark.sql.functions import col, count, sum, window, when, max, min
 
 spark = SparkSession.builder \
     .appName("Analyse préliminaire des données sismiques") \
@@ -13,17 +13,17 @@ events = df_cleaned.filter(col("magnitude") >= threshold)
 print("Événements sismiques détectés:")
 events.show()
 
-activity = df_cleaned.groupBy(window(col("date"), "1 minute")) \
+activity = events.groupBy(window(col("date"), "1 hour")) \
     .agg(
-        count("magnitude").alias("nombre_de_secousses"),
-        sum("magnitude").alias("magnitude_totale")
+        max("magnitude").alias("magnitude_max"),
+        min("magnitude").alias("magnitude_min"),
     ) \
-    .filter(col("nombre_de_secousses") > 0)
-print("Analyse de l'activité sismique par minute:")
-activity.show()
+    .withColumn("amplitude", col("magnitude_max") - col("magnitude_min"))
+print("Analyse de l'activité sismique par heure:")
+activity.orderBy("window.start").show()
 
 threshold2 = 5.0
-significant_activity = activity.filter(col("magnitude_totale") >= threshold2)
+significant_activity = activity.filter(col("amplitude") >= threshold2)
 print("Périodes d'activité sismique importante:")
 significant_activity.show()
 
--- app/exo5.py
+++ app/exo5.py
@@ -1,48 +1,15 @@
 # -*- coding: utf-8 -*-
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count, sum, lag, corr, avg
-from pyspark.sql.window import Window
+from pyspark.sql.functions import col
 
-# Initialiser une session Spark
 spark = SparkSession.builder \
     .appName("Analyse des corrélations des événements sismiques") \
     .getOrCreate()
 
-# Charger les données nettoyées depuis HDFS
 df_cleaned = spark.read.csv("hdfs://namenode:9000/dataset_sismique_cleaned/part-00000-cfda7cb0-2a20-4895-b8c9-9897dad031ed-c000.csv", header=True, inferSchema=True)
 
-# 1. Définir un seuil pour détecter les événements sismiques importants
-threshold_major_event = 5.0  # Seuil pour définir un séisme majeur
+correlation_score = df_cleaned.stat.corr("tension entre plaque", "magnitude")
 
-# 2. Créer une fenêtre temporelle basée sur la date des événements
-# Cette fenêtre ne spécifie pas de frame, juste un ordre par la date
-window_spec = Window.orderBy("date")
+print(f"Score de corrélation entre magnitude et tension entre plaque: {correlation_score}")
 
-# 3. Utiliser la fonction lag() pour obtenir la magnitude de l'événement précédent
-df_cleaned = df_cleaned.withColumn("magnitude_previous", lag("magnitude", 1).over(window_spec))
-
-# 4. Filtrer pour récupérer uniquement les événements majeurs et leurs précédents
-df_major_events = df_cleaned.filter(col("magnitude") >= threshold_major_event)
-
-
-# Calcul de la moyenne
-avg_magnitude_previous = df_major_events.select(avg("magnitude_previous").alias("avg_magnitude_previous")).collect()[0][0]
-
-# Calcul de la médiane (percentile à 50%)
-median_magnitude_previous = df_major_events.approxQuantile("magnitude_previous", [0.5], 0.001)[0]
-
-print(f"Moyenne de la magnitude des événements précédents et les séismes majeurs: {avg_magnitude_previous}")
-print(f"Médiane de la magnitude des événements précédents et les séismes majeurs: {median_magnitude_previous}")
-
-# 6. Visualiser les événements précédant les séismes majeurs et leurs magnitudes
-df_major_events.select("date", "magnitude", "magnitude_previous").show()
-
-# 7. Identifier des séquences de secousses avant les événements majeurs
-df_sequences = df_cleaned.filter(col("magnitude_previous").isNotNull()) \
-    .filter(col("magnitude_previous") < threshold_major_event)  # Filtrer les petits séismes avant des grands
-
-print("Séquences de petits séismes précédant des séismes majeurs:")
-df_sequences.show()
-
-# Stopper la session Spark
 spark.stop()
